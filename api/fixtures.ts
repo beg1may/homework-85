973094dafc79e8fcdaf6ed4d0eb4eecb28ecef79
Silalieva Begimay <begimayomurovna@gmail.com>
2025-05-09 14:01:21 +0600

    ["hw#89 - изменены фикстуры"]

--- api/fixtures.ts
+++ api/fixtures.ts
@@ -10,8 +10,9 @@ const run = async () => {
     const db = mongoose.connection;
 
     try {
-        await db.dropCollection('categories');
-        await db.dropCollection('products');
+        await db.dropCollection('artists');
+        await db.dropCollection('albums');
+        await db.dropCollection('tracks');
         await db.dropCollection('users');
     } catch (e) {
         console.error('Collection were not present, skipping drop');
@@ -20,13 +21,15 @@ const run = async () => {
     const [artistTheWeeknd, artistAdele] = await Artist.create(
         {
             name: "The Weeknd",
-            image: "https://upload.wikimedia.org/wikipedia/commons/2/29/The_Weeknd_2015.jpg",
-            information: "The Weeknd (настоящее имя Абель Макконен Тесфайе) — канадский певец, автор песен и продюсер. Известен своими хитовыми песнями, такими как 'Blinding Lights', 'Can't Feel My Face' и 'Starboy'. Он сочетает элементы поп-музыки, R&B и электроники."
+            image: "fixtures/The_Weeknd.jpeg",
+            information: "The Weeknd (настоящее имя Абель Макконен Тесфайе) — канадский певец, автор песен и продюсер. Известен своими хитовыми песнями, такими как 'Blinding Lights', 'Can't Feel My Face' и 'Starboy'. Он сочетает элементы поп-музыки, R&B и электроники.",
+            isPublished: true,
         },
         {
             name: "Adele",
-            image: "https://upload.wikimedia.org/wikipedia/commons/e/e2/Adele_2016.jpg",
-            information: "Adele (Адель Лори Блу Адкинс) — британская певица и автор песен. Она прославилась благодаря мощным балладам, таким как 'Someone Like You' и 'Hello'. Ее альбомы, такие как '21' и '25', стали мировыми хитами и выиграли множество наград."
+            image: "fixtures/Adele.jpeg",
+            information: "Adele (Адель Лори Блу Адкинс) — британская певица и автор песен. Она прославилась благодаря мощным балладам, таким как 'Someone Like You' и 'Hello'. Ее альбомы, такие как '21' и '25', стали мировыми хитами и выиграли множество наград.",
+            isPublished: false,
         },
     );
 
@@ -35,21 +38,25 @@ const run = async () => {
             name: 'After Hours',
             artist: artistTheWeeknd._id,
             yearOfManufacture: 2020,
+            isPublished: true,
         },
         {
             name: 'Starboy',
             artist: artistTheWeeknd._id,
             yearOfManufacture: 2016,
+            isPublished: true,
         },
         {
             name: '25',
             artist: artistAdele._id,
             yearOfManufacture: 2015,
+            isPublished: true,
         },
         {
             name: '21',
             artist: artistAdele._id,
             yearOfManufacture: 2011,
+            isPublished: false,
         },
     );
 
@@ -59,30 +66,35 @@ const run = async () => {
             album: albumAfterHours._id,
             duration: '3:20',
             numberTrack: 1,
+            isPublished: true,
         },
         {
             name: 'Save Your Tears',
             album: albumAfterHours._id,
             duration: '3:35',
             numberTrack: 2,
+            isPublished: false,
         },
         {
             name: 'Heartless',
             album: albumAfterHours._id,
             duration: '3:18',
             numberTrack: 3,
+            isPublished: true,
         },
         {
             name: 'In Your Eyes',
             album: albumAfterHours._id,
             duration: '3:58',
             numberTrack: 4,
+            isPublished: true,
         },
         {
             name: 'After Hours',
             album: albumAfterHours._id,
             duration: '6:02',
             numberTrack: 5,
+            isPublished: true,
         },
 
         {
@@ -90,30 +102,35 @@ const run = async () => {
             album: albumStarboy._id,
             duration: '3:50',
             numberTrack: 1,
+            isPublished: true,
         },
         {
             name: 'I Feel It Coming',
             album: albumStarboy._id,
             duration: '4:29',
             numberTrack: 2,
+            isPublished: true,
         },
         {
             name: 'Party Monster',
             album: albumStarboy._id,
             duration: '3:50',
             numberTrack: 3,
+            isPublished: true,
         },
         {
             name: 'Reminder',
             album: albumStarboy._id,
             duration: '4:05',
             numberTrack: 4,
+            isPublished: true,
         },
         {
             name: 'Secrets',
             album: albumStarboy._id,
             duration: '4:13',
             numberTrack: 5,
+            isPublished: true,
         },
 
         {
@@ -121,30 +138,35 @@ const run = async () => {
             album: album25._id,
             duration: '3:55',
             numberTrack: 1,
+            isPublished: true,
         },
         {
             name: 'Send My Love (To Your New Lover)',
             album: album25._id,
             duration: '3:43',
             numberTrack: 2,
+            isPublished: false,
         },
         {
             name: 'When We Were Young',
             album: album25._id,
             duration: '4:45',
             numberTrack: 3,
+            isPublished: false,
         },
         {
             name: 'All I Ask',
             album: album25._id,
             duration: '4:30',
             numberTrack: 4,
+            isPublished: true,
         },
         {
             name: 'River Lea',
             album: album25._id,
             duration: '4:40',
             numberTrack: 5,
+            isPublished: true,
         },
 
 
@@ -153,30 +175,35 @@ const run = async () => {
             album: album21._id,
             duration: '3:48',
             numberTrack: 1,
+            isPublished: true,
         },
         {
             name: 'Rumour Has It',
             album: album21._id,
             duration: '3:43',
             numberTrack: 2,
+            isPublished: true,
         },
         {
             name: 'Turning Tables',
             album: album21._id,
             duration: '4:11',
             numberTrack: 3,
+            isPublished: true,
         },
         {
             name: "Don't You Remember",
             album: album21._id,
             duration: '4:03',
             numberTrack: 4,
+            isPublished: true,
         },
         {
             name: 'Set Fire to the Rain',
             album: album21._id,
             duration: '4:02',
             numberTrack: 5,
+            isPublished: true,
         },
     );
 
